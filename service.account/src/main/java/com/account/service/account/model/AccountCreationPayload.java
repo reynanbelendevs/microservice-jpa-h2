c39a63dfc5dad866ceb8b1c018c8854c8329d596
reynanbelendevs <reynanbelen@gmail.com>
2025-01-11 11:57:45 +0800

    inital commit

--- service.account/src/main/java/com/account/service/account/model/AccountCreationPayload.java
+++ service.account/src/main/java/com/account/service/account/model/AccountCreationPayload.java
@@ -1,11 +1,8 @@
 package com.account.service.account.model;
 
-import io.micrometer.common.lang.Nullable;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
--- service.account/src/main/java/com/account/service/account/repository/AccountJpaImplRepository.java
+++ service.account/src/main/java/com/account/service/account/repository/AccountJpaImplRepository.java
@@ -1,11 +1,8 @@
 package com.account.service.account.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 
